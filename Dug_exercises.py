56eba3831c57a0542badad626bf706d07c838d43
Dug <lucadughera@gmail.com>
2024-09-23 15:34:19 +0200

    best split v_3

--- Dug_exercises.py
+++ Dug_exercises.py
@@ -4,7 +4,7 @@ from itertools import combinations
 import pandas as pd
 
 credit_data = np.genfromtxt('data/credit.txt', delimiter=',', skip_header=True)
-credit_data_with_headers = pd.read_csv('data/credit.txt', delim_whitespace=True)
+credit_data_with_headers = pd.read_csv('data/credit.txt', delimiter=',')
 #assignment 1
 
 """gini index for two class
@@ -74,7 +74,7 @@ for index in range(1, len(arr)+1):
     print (rSubset(arr, index))
 
 def best_split_v2(x,y):
-    best_impurity_reduction = 1.1
+    best_impurity_reduction = float('inf')
     best_value = 0
     combinations_of_features = []
     # Initialize an empty list to store the separated column names
@@ -109,5 +109,50 @@ def best_split_v2(x,y):
     else:
         raise ValueError("Arrays must have the same size")
 
+def best_split_v3(x,y):
+    best_impurity_reduction_overall = float('inf')
+    best_value_overall = 0
+    best_split_overall = ''
+    best_left_child_indexes = []
+    best_right_child_indexes = []
+
+    if len(x) == len(y):
+        for split in x.columns:
+            best_impurity_reduction = float('inf')
+            best_value = 0
+            indexes_left_child =[]
+            indexes_right_child = []
+            sorted_values = np.sort(np.unique(x[split]))
+            print(sorted_values)
+            for value_index in range(len(sorted_values - 1)):
+                # follows the x < c instructions, the variable avg is the average of two consecutive numbers
+                avg = sum(sorted_values[value_index:value_index + 2]) / len(sorted_values[value_index:value_index + 2])
+                # select all the indexes where x < c (left child), then select indexes for the right child
+                indexes_left_child = [i for i, value in enumerate(x[split]) if value <= avg]
+                indexes_right_child = list(set(range(len(x[split]))) - set(indexes_left_child))
+                # calculate gini index for the current split, for both children
+                gini_index_left_child = gini_index_calc(y[indexes_left_child])
+                gini_index_right_child = gini_index_calc(y[indexes_right_child])
+                # calculate impurity reduction, lecture 2 slide 12
+                impurity_reduction = gini_index_calc(y) - (
+                            len(y[indexes_left_child]) / len(y) * gini_index_left_child + len(
+                        y[indexes_right_child]) / len(y) * gini_index_right_child)
+                if impurity_reduction < best_impurity_reduction:
+                    best_impurity_reduction = impurity_reduction
+                    best_value = avg
+            if best_impurity_reduction < best_impurity_reduction_overall:
+                best_impurity_reduction_overall = best_impurity_reduction
+                best_value_overall = best_value
+                best_split_overall = split
+                best_left_child_indexes = indexes_left_child
+                best_right_child_indexes = indexes_right_child
+        return best_value_overall, best_split_overall, best_left_child_indexes, best_right_child_indexes
+    else:
+        raise ValueError("Arrays must have the same size")
+
 #best_split_v2(credit_data[:,3],credit_data[:,5])
-best_split_v2(credit_data_with_headers, credit_data[:,5])
+#best_split_v2(credit_data_with_headers, credit_data[:,5])
+print(best_split_v3(credit_data_with_headers.loc[:, credit_data_with_headers.columns != 'class'], credit_data[:,5]))
+
+data_matrix = [[1,0,1,1],[1,0,0,1],[0,1,0,1]]
+labels = [[0,1],[1,1]]
