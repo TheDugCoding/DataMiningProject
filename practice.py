2997a3412e96d0f3def2711b9dac6dd2efda40c5
Riccardo Campanella <cmprcr99d17c349s@unical.it>
2024-09-23 17:49:05 +0200

    add tree grow init

--- practice.py
+++ practice.py
@@ -44,6 +44,32 @@ def bestsplit(x, y):
     
     return best_split 
 
+class Node:
+    def __init__(self, feature, c, prediction, left, right):
+        self.feature = feature
+        self.c = c
+        self.prediction = prediction
+        self.left = left
+        self.right = right 
+
+
+def tree_grow(x, y, nmin, minleaf, nfeat):
+    nodelist = x
+    while len(nodelist) > 0:
+        # early stopping
+        if x < nmin:
+            pass
+        for feature in x:
+            # looking for the best value to split a given attribute
+            c = best_split(x[feature], y)
+
+            # split instances
+            pred_left = y[x <= c]
+            pred_right = y[x > c]   
+
+            #if impurity(i) > 0:
+            
+
 
 if __name__ == "__main__":
     # Read the dataset in the txt file into a 2d-matrix 
