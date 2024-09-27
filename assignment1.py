8f088d2ff0cc03020c738af159b4e5f0d65587b0
Riccardo Campanella <cmprcr99d17c349s@unical.it>
2024-09-27 18:07:51 +0200

    merge code ass1

--- assignment1.py
+++ assignment1.py
@@ -1,28 +1,64 @@
 import numpy as np
 import random
 from collections import Counter
+import pandas as pd
 
-def best_split(x, y):
-    best_impurity_reduction = 1.1
-    best_value = 0
+credit_data_with_headers = pd.read_csv('data/credit.txt', delimiter=',')
+
+def best_split(x, y, minleaf):
+    best_impurity_reduction_overall = float('inf')
+    best_value_overall = 0
+    best_split_overall = ''
+    best_left_child_indexes_overall = []
+    best_right_child_indexes_overall = []
+
     if len(x) == len(y):
-        sorted_values = np.sort(np.unique(x[:, 3]))
-        print(sorted_values)
-        for value_index in range(len(sorted_values-1)):
-            #follows the x < c instructions, the variable avg is the average of two consecutive numbers
-            avg = sum(sorted_values[value_index:value_index+2])/len(sorted_values[value_index:value_index+2])
-            #select all the indexes where x < c (left child), then select indexes for the right child
-            indexes_left_child = [i for i, value in enumerate(x) if value <= avg]
-            indexes_right_child = list(set(range(len(x))) - set(indexes_left_child))
-            #calculate gini index for the current split, for both children
-            gini_index_left_child = gini_index_calc(y[indexes_left_child])
-            gini_index_right_child = gini_index_calc(y[indexes_right_child])
-            #calculate impurity reduction, lecture 2 slide 12
-            impurity_reduction = gini_index_calc(y) - (len(y[indexes_left_child])/len(y) * gini_index_left_child + len(y[indexes_right_child])/len(y) * gini_index_right_child)
-            if impurity_reduction < best_impurity_reduction:
-                best_impurity_reduction = impurity_reduction
-                best_value = avg
-        return best_value, best_impurity_reduction
+        for split in x.columns:
+            best_impurity_reduction = float('inf')
+            best_value = 0
+            best_left_child_indexes = []
+            best_right_child_indexes = []
+            sorted_values = np.sort(np.unique(x[split]))
+            #check that we have enough different values for a split
+            if len(sorted_values) > 1:
+                # check if there are only 2 values, then we don't need to calculate the average
+                if len(sorted_values) == 2:
+                    best_left_child_indexes = x[split][x[split] == sorted_values[0]].index.tolist()
+                    best_right_child_indexes = list(set(x[split].index)- set(best_left_child_indexes))
+                    best_value = sorted_values[0]
+                    gini_index_left_child = gini_index_calc(y[best_left_child_indexes])
+                    gini_index_right_child = gini_index_calc(y[best_right_child_indexes])
+                    best_impurity_reduction = gini_index_calc(y) - (
+                            len(y[best_left_child_indexes]) / len(y) * gini_index_left_child + len(
+                        y[best_right_child_indexes]) / len(y) * gini_index_right_child)
+                else:
+                    for value_index in range(len(sorted_values - 1)):
+                        # follows the x < c instructions, the variable avg is the average of two consecutive numbers
+                        avg = sum(sorted_values[value_index:value_index + 2]) / len(
+                            sorted_values[value_index:value_index + 2])
+                        # select all the indexes where x < c (left child), then select indexes for the right child
+                        indexes_left_child = x[split][x[split] <= avg].index.tolist()
+                        indexes_right_child = list(set(x[split].index)- set(indexes_left_child))
+                        # calculate gini index for the current split, for both children
+                        gini_index_left_child = gini_index_calc(y[indexes_left_child])
+                        gini_index_right_child = gini_index_calc(y[indexes_right_child])
+                        # calculate impurity reduction, lecture 2 slide 12
+                        impurity_reduction = gini_index_calc(y) - (
+                                len(y[indexes_left_child]) / len(y) * gini_index_left_child + len(
+                            y[indexes_right_child]) / len(y) * gini_index_right_child)
+                        if impurity_reduction < best_impurity_reduction and len(indexes_left_child) > minleaf and len(
+                                indexes_right_child) > minleaf:
+                            best_impurity_reduction = impurity_reduction
+                            best_value = avg
+                            best_left_child_indexes = indexes_left_child
+                            best_right_child_indexes = indexes_right_child
+                if best_impurity_reduction < best_impurity_reduction_overall:
+                    best_impurity_reduction_overall = best_impurity_reduction
+                    best_value_overall = best_value
+                    best_split_overall = split
+                    best_left_child_indexes_overall = best_left_child_indexes
+                    best_right_child_indexes_overall = best_right_child_indexes
+        return best_left_child_indexes_overall, best_right_child_indexes_overall, best_split_overall, best_value_overall
     else:
         raise ValueError("Arrays must have the same size")
     
@@ -30,44 +66,73 @@ def gini_index_calc(x):
     gini_index = 1
     for class_name, value in Counter(x).items():
         gini_index *= (value/len(x))
-    print(gini_index)
     return gini_index
 
 def impurity(x):
-    sum = 0
-    for i in x:
-        sum += i
-    prob_0 = sum/len(x)
-    prob_1 = 1-prob_0
-    return prob_0 * prob_1
+    if len(x) > 0:
+        sum = 0
+        for i in x:
+            sum += i
+        prob_0 = sum/len(x)
+        prob_1 = 1-prob_0
+        return prob_0 * prob_1
+    else:
+        return 0
+
+class Node:
+    def __init__(self, feature=None, threshold=None, predicted_class=None, left=None, right=None, parent=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.predicted_class = predicted_class
+        self.left = left
+        self.right = right
+        self.parent = parent
 
 def tree_grow(x, y, nmin, minleaf, nfeat):
-    nodelist = x
-    tree = {}
-    level = 0
-    # possible nodes to check must exist
+    node = 1
+    nodelist = [Node()]
+    Tree = {}
+
+    # The tree stops to grow when we split all the nodes 
     while len(nodelist) > 0:
-        for i in nodelist:
-            # remove current node from nodes to check
-            nodelist = nodelist.remove(i)
-            # check if impurity of current node is not 0, else it cannot be split and is leaf node
-            if impurity(i) > 0:
-                if len(i) >= nmin:
-                    # randomly select n number of candidate splits
-                    candidate_splits = random.sample(i, nfeat)
-                    # calculate best split and impurity reduction
-                    child_node_left, child_node_right, reduction = best_split(candidate_splits, y)
-                    # add child nodes to be checked to nodelist
-                    nodelist = nodelist.append(child_node_left, child_node_right)
-                # return nodes in tree (WIP)
-                tree[level] = child_node_left, child_node_right
-            else:
-                tree[level] = i
-    return tree
+        current_node = nodelist.pop(0)
+        current_node_instances = [current_node.left + current_node.right].tolist()
+        classes = y.iloc[[i for i in range(x.shape[0]) if current_node.parent == None]]
 
-def tree_pred():
-    print('tree')
+        # Can't split leaf nodes with zero impurity 
+        if impurity(classes) > 0:
+            # early stopping: pure node
+            if len(current_node.left) >= nmin or len(current_node.left) >= nmin:
+                # randomly select nfeat number of columns
+                candidate_splits = current_node.sample(n=nfeat, axis='columns')
+                # calculate best split and impurity reduction to get child nodes
+                child_node_left, child_node_right, split, value = best_split(candidate_splits, classes, minleaf)
+                # add rows of child nodes to be checked to nodelist
+                nodelist.append(x.iloc[child_node_left])
+                nodelist.append(x.iloc[child_node_right])
 
-data_matrix = [[1,0,1,1],[1,0,0,1],[0,1,0,1]]
-labels = [[0,1],[1,1]]
-tree_grow(data_matrix, labels, 2, 2, 2)
+                if (len(child_node_left) + len(child_node_right)) == 0:
+                    Tree[node, "leaf"] = current_node_instances
+                else:
+                    Tree[node] = current_node_instances
+                node += 1
+        else:
+            if len(current_node.left) > 0 or len(current_node.left) > 0:
+                Tree[node, "leaf"] = current_node_instances
+                node += 1
+    return Tree
+
+def tree_grow_b(x, y, nmin, minleaf, nfeat, m):
+    # assignment states trees must be in list
+    trees = []
+    for i in range(m):
+        trees.append(tree_grow(x, y, nmin, minleaf, nfeat))
+    return trees
+
+def tree_pred(x, tr):
+    predicted_labels = ''
+    return predicted_labels
+
+#print(best_split(credit_data_with_headers.loc[:, credit_data_with_headers.columns != 'class'], credit_data_with_headers['class'], 2))
+Tree = tree_grow_b(credit_data_with_headers.loc[:, credit_data_with_headers.columns != 'class'], credit_data_with_headers['class'], 2, 2, 5, 6)
+print(Tree)
