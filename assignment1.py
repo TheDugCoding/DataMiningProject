5e55addb935f70b24f71be68db6181a73bcd9e40
Dug <lucadughera@gmail.com>
2024-10-11 12:00:45 +0200

    fixes

--- assignment1.py
+++ assignment1.py
@@ -19,12 +19,153 @@ from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 """
+Uncomment this part to run the examples (need to download the datasets listed in the README file)
 credit_data_with_headers = pd.read_csv('data/credit.txt', delimiter=',')
 indians = pd.read_csv('data/indians.txt', delimiter=',', names=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'])
 """
 
 #True use multiprocessing, False don't use multiprocessing
 MULTIPROCESSING = True
+
+def tree_grow(x: pd.DataFrame, y, nmin, minleaf, nfeat):
+    """
+    :param x: rows of the dataset used for creating the tree
+    :param y: rows of the target features used for creating the tree
+    :param nmin: the number of observations that a node must contain at least, for it to be allowed to be split
+    :param minleaf: minimum number of observations required for a leaf node
+    :param nfeat: number of features that should be considered for each split
+    :return: the function returns a binary tree
+    """
+    if not x.empty:
+        root = Node(x.index)
+        nodelist = [root]
+        leaves = []
+        i = 0
+
+        # tree grow stops when we split all the nodes
+        while i < len(nodelist):
+            # visit all the nodes in the list, for optimization we don't use 'pop' we just iterate over the nodes
+            current_node = nodelist[i]
+
+            # store the node instances
+            current_node_instances = current_node.instances
+
+            # store node in the tree before splitting
+            labels = y.iloc[current_node_instances]
+
+            # avoid splitting leaf nodes with zero impurity and check that there are enough observations for a split
+            if impurity(labels) > 0 and len(current_node.instances) >= nmin:
+
+                # random sample nfeat number of columns
+                candidate_features = np.random.choice(x.columns, size=nfeat, replace=False)
+
+                # calculate best split and impurity reduction to get child nodes, (if a split is not found feature = None)
+                left, right, feature, threshold = best_split(x.loc[current_node_instances, candidate_features], labels, minleaf)
+
+                # store current node info, if it is not a leaf
+                if feature:
+                    current_node.left = Node(left)
+                    current_node.right = Node(right)
+                    # update list
+                    nodelist.append(current_node.left)
+                    nodelist.append(current_node.right)
+                    current_node.threshold = threshold
+                    current_node.feature = feature
+                else:
+                    leaves.append(current_node)
+                current_node.predicted_class = statistics.mode(labels)
+
+            else:
+                # return the final prediction of the leaf node
+                current_node.predicted_class = statistics.mode(labels)
+                leaves.append(current_node)
+            i+= 1
+        return Tree(root, leaves)
+    else:
+        raise ValueError("x is empty")
+
+def tree_pred(x, tr):
+    """
+    :param x: rows of the dataset used to make a prediction
+    :param tr: the tree that we are using for the prediction
+    :return: a single dimensional array containing the prediction of the tree
+    """
+    predicted_labels = []
+    for index, row in x.iterrows():
+        current_node = tr.root
+        # a leaf node doesn't contain a feature
+        while current_node.feature:
+            # left branch
+            if row[current_node.feature] < current_node.threshold:
+                current_node = current_node.left
+            else:
+                # right branch
+                current_node = current_node.right
+        predicted_labels.append(current_node.predicted_class)
+
+    return predicted_labels
+
+def tree_grow_b(x, target_feature, nmin, minleaf, nfeat, m):
+    """
+    :param x: rows of the dataset used for creating the tree
+    :param target_feature: name of the feature used for classification
+    :param nmin: the number of observations that a node must contain at least, for it to be allowed to be split.
+    :param minleaf: minimum number of observations required for a leaf node
+    :param nfeat: number of features used to select the best split
+    :param m: number of trees to be created
+    :return: the function return a list of trees
+    """
+    trees = []
+    random_indexes_with_replacement = np.random.choice(x.index.tolist(), size=(m,len(x)), replace=True)
+
+    if MULTIPROCESSING:
+        with ProcessPoolExecutor() as executor:
+            futures = []
+            # using parallelization to speed up the process, in case parallelization doesn't work change value of the variable MULTIPROCESSING to False
+            for i in range(m):
+                future = executor.submit(tree_grow, x.loc[random_indexes_with_replacement[i], x.columns != target_feature].reset_index(drop=True), x.loc[random_indexes_with_replacement[i], target_feature].reset_index(drop=True), nmin, minleaf, nfeat)
+                futures.append(future)
+
+            for future in tqdm(as_completed(futures), total=len(futures)):
+                trees.append(future.result())
+
+    else:
+        for i in tqdm(range(m)):
+            tree = tree_grow(     x.loc[random_indexes_with_replacement[i], x.columns != target_feature].reset_index(
+                                         drop=True),
+                                     x.loc[random_indexes_with_replacement[i], target_feature].reset_index(drop=True), nmin,
+                                     minleaf, nfeat)
+            trees.append(tree)
+
+
+    return trees
+
+def tree_pred_b(x, tr):
+    """
+    :param x: rows of the dataset used to make a prediction
+    :param tr: a list of trees that we are using for predictions
+    :return: a list containing the final predictions, where each prediction is made
+    by selecting the class that received the most votes.
+    """
+    majority_votes = {}
+    predicted_labels = []
+
+    for tree in tqdm(tr, desc="Processing Trees Predictions", unit="tree"):
+        predicted_labels.append(tree_pred(x, tree))
+
+    # loop over the list of predicted labels (one list for each tree)
+    for tree_predictions in tqdm(predicted_labels, desc="Processing Predictions", unit="set"):
+        # loop over the individual predictions in a tree
+        for i in range(len(tree_predictions)):
+            if i not in majority_votes:
+                majority_votes[i] = 0
+            # add 1 for '1', subtract 1 for '0'
+            majority_votes[i] += 1 if tree_predictions[i] == 1 else -1
+
+    # convert the majority votes dictionary to a list of predictions
+    final_predictions = [1 if majority_votes[i] > 0 else 0 for i in range(len(majority_votes))]
+
+    return final_predictions
 
 def best_split(x, y, minleaf):
     """
@@ -99,148 +240,6 @@ class Tree:
     def __init__(self, root = None, leaves = None):
         self.root = root
         self.leaves = leaves
-
-def tree_grow(x: pd.DataFrame, y, nmin, minleaf, nfeat):
-    """
-    :param x: rows of the dataset used for creating the tree
-    :param y: rows of the target features used for creating the tree
-    :param nmin: the number of observations that a node must contain at least, for it to be allowed to be split
-    :param minleaf: minimum number of observations required for a leaf node
-    :param nfeat: number of features that should be considered for each split
-    :return: the function returns a binary tree
-    """
-    if not x.empty:
-        root = Node(x.index)
-        nodelist = [root]
-        leaves = []
-        i = 0
-
-        # tree grow stops when we split all the nodes
-        while i < len(nodelist):
-            # visit all the nodes in the list, for optimization we don't use 'pop' we just iterate over the nodes
-            current_node = nodelist[i]
-
-            # store the node instances
-            current_node_instances = current_node.instances
-
-            # store node in the tree before splitting
-            labels = y.iloc[current_node_instances]
-
-            # avoid splitting leaf nodes with zero impurity and check that there are enough observations for a split
-            if impurity(labels) > 0 and len(current_node.instances) >= nmin:
-
-                # random sample nfeat number of columns
-                candidate_features = np.random.choice(x.columns, size=nfeat, replace=False)
-
-                # calculate best split and impurity reduction to get child nodes, (if a split is not found feature = None)
-                left, right, feature, threshold = best_split(x.loc[current_node_instances, candidate_features], labels, minleaf)
-
-                # store current node info, if it is not a leaf
-                if feature:
-                    current_node.left = Node(left)
-                    current_node.right = Node(right)
-                    # update list
-                    nodelist.append(current_node.left)
-                    nodelist.append(current_node.right)
-                    current_node.threshold = threshold
-                    current_node.feature = feature
-                else:
-                    leaves.append(current_node)
-                current_node.predicted_class = statistics.mode(labels)
-
-            else:
-                # return the final prediction of the leaf node
-                current_node.predicted_class = statistics.mode(labels)
-                leaves.append(current_node)
-            i+= 1
-        return Tree(root, leaves)
-    else:
-        raise ValueError("x is empty")
-
-
-def tree_grow_b(x, target_feature, nmin, minleaf, nfeat, m):
-    """
-    :param x: rows of the dataset used for creating the tree
-    :param target_feature: name of the feature used for classification
-    :param nmin: the number of observations that a node must contain at least, for it to be allowed to be split.
-    :param minleaf: minimum number of observations required for a leaf node
-    :param nfeat: number of features used to select the best split
-    :param m: number of trees to be created
-    :return: the function return a list of trees
-    """
-    trees = []
-    random_indexes_with_replacement = np.random.choice(x.index.tolist(), size=(m,len(x)), replace=True)
-
-    if MULTIPROCESSING:
-        with ProcessPoolExecutor() as executor:
-            futures = []
-            # using parallelization to speed up the process, in case parallelization doesn't work change value of the variable MULTIPROCESSING to False
-            for i in range(m):
-                future = executor.submit(tree_grow, x.loc[random_indexes_with_replacement[i], x.columns != target_feature].reset_index(drop=True), x.loc[random_indexes_with_replacement[i], target_feature].reset_index(drop=True), nmin, minleaf, nfeat)
-                futures.append(future)
-
-            for future in tqdm(as_completed(futures), total=len(futures)):
-                trees.append(future.result())
-
-    else:
-        for i in tqdm(range(m)):
-            tree = tree_grow(     x.loc[random_indexes_with_replacement[i], x.columns != target_feature].reset_index(
-                                         drop=True),
-                                     x.loc[random_indexes_with_replacement[i], target_feature].reset_index(drop=True), nmin,
-                                     minleaf, nfeat)
-            trees.append(tree)
-
-
-    return trees
-
-def tree_pred(x, tr):
-    """
-    :param x: rows of the dataset used to make a prediction
-    :param tr: the tree that we are using for the prediction
-    :return: a single dimensional array containing the prediction of the tree
-    """
-    predicted_labels = []
-    for index, row in x.iterrows():
-        current_node = tr.root
-        # a leaf node doesn't contain a feature
-        while current_node.feature:
-            # left branch
-            if row[current_node.feature] < current_node.threshold:
-                current_node = current_node.left
-            else:
-                # right branch
-                current_node = current_node.right
-        predicted_labels.append(current_node.predicted_class)
-
-    return predicted_labels
-
-
-def tree_pred_b(x, tr):
-    """
-    :param x: rows of the dataset used to make a prediction
-    :param tr: a list of trees that we are using for predictions
-    :return: a list containing the final predictions, where each prediction is made
-    by selecting the class that received the most votes.
-    """
-    majority_votes = {}
-    predicted_labels = []
-
-    for tree in tqdm(tr, desc="Processing Trees Predictions", unit="tree"):
-        predicted_labels.append(tree_pred(x, tree))
-
-    # loop over the list of predicted labels (one list for each tree)
-    for tree_predictions in tqdm(predicted_labels, desc="Processing Predictions", unit="set"):
-        # loop over the individual predictions in a tree
-        for i in range(len(tree_predictions)):
-            if i not in majority_votes:
-                majority_votes[i] = 0
-            # add 1 for '1', subtract 1 for '0'
-            majority_votes[i] += 1 if tree_predictions[i] == 1 else -1
-
-    # convert the majority votes dictionary to a list of predictions
-    final_predictions = [1 if majority_votes[i] > 0 else 0 for i in range(len(majority_votes))]
-
-    return final_predictions
 
 if __name__ == '__main__':
 
