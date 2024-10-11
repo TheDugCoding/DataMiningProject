4c83110c2da22f12eb9cde867627a4bffe957369
Dug <lucadughera@gmail.com>
2024-10-11 10:57:06 +0200

    first deliverable version

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 This repository contains our code for the assignments for the course INFOMDM Data Mining.
 
 ## Instructions
-Due to parallelization, to call the functions the code must be structured in the following way:
+Due to parallelization, to call functions the code must be structured in the following way:
 
 ```
 if __name__ == '__main__':
     #code
 ```
 
-if the parallelization doesn't work, please change the value of MULTIPROCESSING to False
+if parallelization doesn't work, please change the value of MULTIPROCESSING to False
+
+There are some code examples  based on the Indian dataset
+and the credit dataset provided in the preparatory assignments.
--- assignment1.py
+++ assignment1.py
@@ -2,33 +2,16 @@ import numpy as np
 import pandas as pd
 import statistics
 
-from statsmodels.stats.contingency_tables import mcnemar
 from tqdm import tqdm
 
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 credit_data_with_headers = pd.read_csv('data/credit.txt', delimiter=',')
 indians = pd.read_csv('data/indians.txt', delimiter=',', names=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'])
-eclipse_2 = pd.read_csv('data/eclipse-metrics-packages-2.0.csv', delimiter=';')
-eclipse_3 = pd.read_csv('data/eclipse-metrics-packages-3.0.csv', delimiter=';')
-diabetes = pd.read_csv('data/diabetes.csv', delimiter=',')
+
 
 #True use multiprocessing, False don't use multiprocessing
 MULTIPROCESSING = True
-
-# clean data for part 2
-features_data = ['pre', 'post', 'FOUT', 'MLOC', 'NBD', 'PAR', 'VG', 'NOF', 'NOM', 'NSF', 'NSM', 'ACD', 'NOI', 'NOT', 'TLOC', 'NOCU']
-keep_col_list = []
-for feature in features_data:
-    for col in eclipse_2.columns: 
-        if col.startswith(feature):
-            keep_col_list.append(col)
-training_data = eclipse_2[keep_col_list].astype('float64')
-test_data = eclipse_3[keep_col_list].astype('float64')
-training_data.loc[training_data['post'] > 0, 'post'] = 1
-test_data.loc[test_data['post'] > 0, 'post'] = 1
-training_features = training_data.drop('post', axis=1)
-test_features = test_data.drop('post', axis=1)
 
 def best_split(x, y, minleaf):
     """
@@ -246,101 +229,10 @@ def tree_pred_b(x, tr):
 
     return final_predictions
 
-def print_tree(node, level=0, side="root"):
-    """ Recursively print the structure of the decision tree. """
-    if node is None:
-        print("The tree is empty.")
-        return
-
-    indent = "   " * level  # Indentation for visual representation
-
-    # Check if node is a leaf
-    if node != []:
-        if node.left is None and node.right is None:
-            # Leaf node: print predicted class and number of instances
-            print(f"{indent}- {side} [Leaf] Predicted class: {node.predicted_class}, Instances: {len(node.instances)}")
-        else:
-            # Internal node: print splitting feature and threshold
-            print(f"{indent}- {side} [Node] Feature: {node.feature}, Threshold: {node.threshold}, Instances: {len(node.instances)}")
-
-            # Recursively print the left and right subtrees
-            if node.left is not None:
-                print_tree(node.left, level + 1, "left")
-            else:
-                print(f"{indent}   - left [Empty]")  # Show if the left child is missing
-
-            if node.right is not None:
-                print_tree(node.right, level + 1, "right")
-            else:
-                print(f"{indent}   - right [Empty]")  # Show if the right child is missing
-
-def print_tree_recursive(node, level=0, side="root", split_level=3):
-    """ Recursively print the structure of the decision tree. """
-    if node is None:
-        print("The tree is empty.")
-        return
-
-    indent = "   " * level  # Indentation for visual representation
-
-    # Check if node is a leaf
-    if node != [] and split_level > 0:
-        if node.left is None and node.right is None:
-            # Leaf node: print predicted class and number of instances
-            print(f"{indent}- {side} [Leaf] Predicted class: {node.predicted_class}, Instances: {len(node.instances)}")
-        else:
-            # Internal node: print splitting feature and threshold
-            print(f"{indent}- {side} [Node] Feature: {node.feature}, Threshold: {node.threshold}, Instances: {len(node.instances)}")
-
-            # Recursively print the left and right subtrees
-            print_tree_recursive(node.left, level + 1, "left", split_level - 1)
-            print_tree_recursive(node.right, level + 1, "right", split_level - 1)
-
-
-def print_tree(single_credit=False, ensamble_credit=False, single_indians=False, single_eclipse=False, bagging=False, random_forest=False):
-    trees_to_process = [
-        (single_credit, "Single tree - Credit data"),
-        (single_indians, "Single tree - Indians data"),
-        (single_eclipse, "Single tree - Eclipse data"),
-        (ensamble_credit, "Ensamble credit"),
-        (bagging, "Bagging:"),
-        (random_forest, "Random forest")
-    ]
-
-    for tree, message in trees_to_process:
-        if tree:
-            if isinstance(tree, list):
-                print(message)
-                for t in tree:
-                    print_tree_recursive(t)
-            else:
-                print(message)
-                print_tree_recursive(tree)
-
-#print_tree(single_credit=single_tree, ensamble_credit=False, single_indians=indians_tree, single_eclipse=train_tree, bagging=False, random_forest=False)
-#print_tree(single_credit=single_tree, ensamble_credit=ensamble_tree, single_indians=False, single_eclipse=False, bagging=False, random_forest=False)
-#print_tree(single_credit=False, ensamble_credit=False, single_indians=False, single_eclipse=train_tree, bagging=False, random_forest=False)
-
-def mcnemar_test(y_true, y_pred1, y_pred2):
-    # We need to compare the predictions to build the contingency table
-    #correct_bagging = (test_bagging == test_data['post'])
-    correct_rf = (test_random == test_data['post'])
-
-    correct1 = (y_pred1 == y_true)
-    correct2 = (y_pred2 == y_true)
-
-    # Contigency table
-    table = np.zeros((2, 2))
-    table[0, 0] = np.sum(correct1 & correct2)   # Both correct
-    table[0, 1] = np.sum(~correct1 & correct2)  # Model 2 correct, Model 1 wrong
-    table[1, 0] = np.sum(correct1 & ~correct2)  # Model 1 correct, Model 2 wrong
-    table[1, 1] = np.sum(~correct1 & ~correct2) # Both wrong
-
-    print(table)
-
-    result = mcnemar(table, exact=True) # exact must be true as the 'b' value in the contigency table of baggins vs random is <= 25 i.e. not possible to use asymptotic test.
-    return result
-
 if __name__ == '__main__':
+
+    '''
+    code Examples
     #print(best_split(credit_data_with_headers.loc[:, credit_data_with_headers.columns != 'class'], credit_data_with_headers['class'], 2))
 
     single_tree = tree_grow(credit_data_with_headers.loc[:, credit_data_with_headers.columns != 'class'], credit_data_with_headers['class'], 2, 2, 5)
@@ -356,9 +248,10 @@ if __name__ == '__main__':
     #test prediction_b
     print('\n\n--prediction all trees')
     predictions = tree_pred_b(credit_data_with_headers.loc[:, credit_data_with_headers.columns != 'class'].iloc[-2:], ensamble_tree)
-    # print(predictions)
+    print(predictions)
 
     # test indians confusion matrix
+    print('\n\n--prediction single trees Indian database')
     indians_tree = tree_grow(indians.drop('i', axis=1), indians['i'], 20, 5, 8)
     indians_pred = tree_pred(indians.drop('i', axis=1), indians_tree)
     pred_true = {'00': 0, '10': 0, '01': 0, '11': 0}
@@ -375,114 +268,4 @@ if __name__ == '__main__':
             else:
                 pred_true['01'] += 1
     print(pred_true)
-
-    # training - single tree
-    print('\n\n--prediction single tree dataset')
-    train_tree = tree_grow(training_features, training_data['post'], 15, 5, 41)
-    test_tree = tree_pred(test_features, train_tree)
-
-    confusion_matrix = {'TN': 0, 'FP': 0, 'FN': 0, 'TP': 0}
-    for i in range(len(test_tree)):
-        # check whether pred (tree) and true data are equal
-        if test_tree[i] == 0:
-            if test_data['post'][i] == 0:
-                confusion_matrix['TN'] += 1
-            if test_data['post'][i] > 0:
-                confusion_matrix['FN'] += 1
-        if test_tree[i] > 0:
-            if test_data['post'][i] > 0:
-                confusion_matrix['TP'] += 1
-            if test_data['post'][i] == 0:
-                confusion_matrix['FP'] += 1
-
-    accuracy = (confusion_matrix['TN'] + confusion_matrix['TP']) / len(test_tree)
-    precision = confusion_matrix['TP'] / (confusion_matrix['TP'] + confusion_matrix['FP'])
-    recall = confusion_matrix['TP'] / (confusion_matrix['TP'] + confusion_matrix['FN'])
-    print('single tree', accuracy, precision, recall)
-    print(confusion_matrix)
-
-
-    # training - bagging
-    print('\n\n--prediction bagging dataset')
-    train_bagging = tree_grow_b(training_data, 'post', 15, 5, 41, 100)
-    test_bagging = tree_pred_b(test_data, train_bagging)
-    confusion_matrix = {'TN': 0, 'FP': 0, 'FN': 0, 'TP': 0}
-    for i in range(len(test_bagging)):
-        # check whether pred (tree) and true data are equal
-        if test_bagging[i] == 0:
-            if test_data['post'][i] == 0:
-                confusion_matrix['TN'] += 1
-            if test_data['post'][i] > 0:
-                confusion_matrix['FN'] += 1
-        if test_bagging[i] > 0:
-            if test_data['post'][i] > 0:
-                confusion_matrix['TP'] += 1
-            if test_data['post'][i] == 0:
-                confusion_matrix['FP'] += 1
-
-    accuracy = (confusion_matrix['TN'] + confusion_matrix['TP']) / len(test_tree)
-    precision = confusion_matrix['TP'] / (confusion_matrix['TP'] + confusion_matrix['FP'])
-    recall = confusion_matrix['TP'] / (confusion_matrix['TP'] + confusion_matrix['FN'])
-    print('bagging', accuracy, precision, recall)
-    print(confusion_matrix)
-
-    # training - random forest
-    print('\n\n--prediction random forest dataset')
-    train_random = tree_grow_b(training_data, 'post', 15, 5, 6, 100)
-    test_random = tree_pred_b(test_data, train_random)
-    confusion_matrix = {'TN': 0, 'FP': 0, 'FN': 0, 'TP': 0}
-    for i in range(len(test_random)):
-        # check whether pred (tree) and true data are equal
-        if test_random[i] == 0:
-            if test_data['post'][i] == 0:
-                confusion_matrix['TN'] += 1
-            if test_data['post'][i] > 0:
-                confusion_matrix['FN'] += 1
-        if test_random[i] > 0:
-            if test_data['post'][i] > 0:
-                confusion_matrix['TP'] += 1
-            if test_data['post'][i] == 0:
-                confusion_matrix['FP'] += 1
-
-    accuracy = (confusion_matrix['TN'] + confusion_matrix['TP']) / len(test_tree)
-    precision = confusion_matrix['TP'] / (confusion_matrix['TP'] + confusion_matrix['FP'])
-    recall = confusion_matrix['TP'] / (confusion_matrix['TP'] + confusion_matrix['FN'])
-    print('random forest', accuracy, precision, recall)
-    print(confusion_matrix)
-
-    # Pairwise comparisons (without correction)
-    mcnemar_single_tree_bagging = mcnemar_test(test_data['post'], test_tree, test_bagging)
-    mcnemar_single_tree_rf = mcnemar_test(test_data['post'], test_tree, test_random)
-    mcnemar_bagging_rf = mcnemar_test(test_data['post'], test_bagging, test_random)
-
-
-    # Bonferroni correction: 3 comparisons, so we divide the alpha level by 3
-    alpha = 0.05
-    bonferroni_alpha = alpha / 3
-    significance_levels = [alpha, bonferroni_alpha]
-
-    for alpha_value in significance_levels:
-        print(f"McNemar's test Significance level:  alpha = {alpha_value:.4f}\n")
-
-        print(f"Single Tree vs Bagging: p-value = {mcnemar_single_tree_bagging.pvalue:.6f}, chi-square = {mcnemar_single_tree_bagging.statistic}")
-        if mcnemar_single_tree_bagging.pvalue < alpha_value:
-            print(f"Significant difference in accuracy between the models.\nReject the Null Hypothesis: it is very unlikely ({mcnemar_single_tree_bagging.pvalue}<{alpha_value:.4f}) to be true\n")
-        else:
-            print("No significant difference in accuracy between the models.\nFail to reject the Null Hypothesis: insufficient evidence to conclude that the null hypothesis is false!\n")
-
-        print(f"Single Tree vs Random Forest: p-value = {mcnemar_single_tree_rf.pvalue:.6f}, chi-square = {mcnemar_single_tree_rf.statistic}")
-        if mcnemar_single_tree_rf.pvalue < alpha_value:
-            print(f"Significant difference in accuracy between the models.\nReject the Null Hypothesis: it is very unlikely ({mcnemar_single_tree_rf.pvalue}<{alpha_value:.4f}) to be true\n")
-        else:
-            print("No significant difference in accuracy between the models.\nFail to reject the Null Hypothesis: insufficient evidence to conclude that the null hypothesis is false!\n")
-
-        print(f"Bagging vs Random Forest: p-value = {mcnemar_bagging_rf.pvalue:.6f}, chi-square = {mcnemar_bagging_rf.statistic}")
-        if mcnemar_bagging_rf.pvalue < alpha_value:
-            print(f"Significant difference in accuracy between the models.\nReject the Null Hypothesis: it is very unlikely ({mcnemar_bagging_rf.pvalue}<{alpha_value:.4f}) to be true\n")
-        else:
-            print("No significant difference in accuracy between the models.\nFail to reject the Null Hypothesis: insufficient evidence to conclude that the null hypothesis is false!\n")
-
-
-
-
-
+    '''
