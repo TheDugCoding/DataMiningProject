483533ce9ea6cd434e23cdfd8b340c05e0927196
Dug <lucadughera@gmail.com>
2024-10-17 16:55:51 +0200

    data prepared

---
+++ assignment_2/assignment_2.py
@@ -0,0 +1,46 @@
+import os
+
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+data_folder = os.path.join(os.getcwd(), 'data\\')
+training_folders = ['fold1', 'fold2', 'fold3', 'fold4']
+testing_folders = ['fold5']
+
+
+# Function to load reviews and labels from a folder
+def load_data_from_folder(main_folder,  folders, label):
+    reviews = []
+    labels = []
+
+    for fold in folders:
+        fold_path = os.path.join(main_folder, fold)
+        for file_name in os.listdir(fold_path):
+            if file_name.endswith('.txt'):
+                file_path = os.path.join(fold_path, file_name)
+
+                # Read the content of the txt file
+                with open(file_path, 'r', encoding='utf-8') as file:
+                    review = file.read().strip()
+                    reviews.append(review)
+                    labels.append(label)
+
+    return pd.DataFrame({'review': reviews, 'label': labels})
+
+
+# load the data for negative review and put a 0
+deceptive_reviews_training = load_data_from_folder(f'{data_folder}deceptive_from_MTurk', training_folders, label=0)
+deceptive_reviews_testing = load_data_from_folder(f'{data_folder}deceptive_from_MTurk', testing_folders, label=0)
+
+#  load the data for negative review and put a 1
+truthful_reviews_training = load_data_from_folder(f'{data_folder}truthful_from_Web', training_folders, label=1)
+truthful_reviews_testing =  load_data_from_folder(f'{data_folder}truthful_from_Web', testing_folders, label=1)
+
+# Combine the two datasets into one DataFrame
+combined_reviews_training = pd.concat([deceptive_reviews_training, truthful_reviews_training], ignore_index=True)
+combined_reviews_testing = pd.concat([deceptive_reviews_testing, truthful_reviews_testing], ignore_index=True)
