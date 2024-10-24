f9ab8710bad58541585a98cf96633cb67f45052c
Riccardo Campanella <cmprcr99d17c349s@unical.it>
2024-10-24 19:42:06 +0200

    add new feature method

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -1,9 +1,14 @@
 import os
+import string
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_selection import mutual_info_classif, SelectKBest
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+nltk.download('punkt_tab')
 
 
 data_folder = os.path.join(os.getcwd(), 'data\\')
@@ -44,32 +49,32 @@ combined_reviews_training = pd.concat([deceptive_reviews_training, truthful_revi
 combined_reviews_testing = pd.concat([deceptive_reviews_testing, truthful_reviews_testing], ignore_index=True)
 
 # pre-processing
-reviews_training = []
-for i in range(len(combined_reviews_training)):
+def preprocess_text(text):
     # make string lowercase and strip punctuation
-    review = combined_reviews_training['review'][i].translate(str.maketrans('', '', string.punctuation)).lower()
+    text = text.translate(str.maketrans('', '', string.punctuation)).lower()
     # remove numbers
-    review = ''.join([i for i in review if not i.isdigit()])
+    text = ''.join([i for i in text if not i.isdigit()])
+    # tokenize
+    word_tokens = word_tokenize(text)
     # remove stopwords
-    word_tokens = word_tokenize(review)
-    review = [w for w in word_tokens if not w.lower() in stop_words]
-    reviews_training.append(' '.join(review))
+    text = [w for w in word_tokens if not w.lower() in stopwords]
+    return ' '.join(text)
 
-X_train = pd.DataFrame(reviews_training, columns=['review'])
-y_train = combined_reviews_training['label']
+X_train = combined_reviews_training['review'].apply(preprocess_text)
+y_train = combined_reviews_training['label'].apply(preprocess_text)
 X_test = combined_reviews_testing['review']
 y_test = combined_reviews_testing['label']
 
 # feature selection
 n_features = 10
 feature_selector = SelectKBest(score=mutual_info_classif, k=n_features)
-X_train = feature_selector.fit_transform(X_train, y_train)
-X_test = feature_selector.transform(X_test, y_test)
+X_train_selected = feature_selector.fit_transform(X_train, y_train)
+X_test_selected = feature_selector.transform(X_test, y_test)
 
 # Print mutual information scores
-if hasattr(X, 'columns'):  # For pandas DataFrame
+if hasattr(X_train, 'columns'):  # For pandas DataFrame
     feature_mask = feature_selector.get_support()
-    selected_features = X.columns[feature_mask].tolist()
+    selected_features = X_train.columns[feature_mask].tolist()
     mi_scores = feature_selector.scores_
     
     print("\nMutual Information scores for selected features:")
