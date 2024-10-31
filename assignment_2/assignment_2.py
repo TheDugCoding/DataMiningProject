f0db8b76c44d11e3abfcc8d8af82d66edb9a0d76
Annebelle Olminkhof <20630578+annebelleo@users.noreply.github.com>
2024-10-31 15:42:12 +0100

    list important features

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -14,8 +14,6 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 data_folder = os.path.join(os.getcwd(), 'assignment_2/data/')
 training_folders = ['fold1', 'fold2', 'fold3', 'fold4']
 testing_folders = ['fold5']
-n_grams_range = [(1,1), (1,2), (2,2)]
-removing_words = [2, 4, 6, 8, 10]
 
 # Function to load reviews and labels from a folder
 def load_data_from_folder(main_folder,  folders, label):
@@ -49,27 +47,39 @@ combined_reviews_training = pd.concat([deceptive_reviews_training, truthful_revi
 combined_reviews_testing = pd.concat([deceptive_reviews_testing, truthful_reviews_testing], ignore_index=True)
 
 # pre-processing
-for n_gram in n_grams_range:
-    for words_to_remove in removing_words:
-        def transform(train, test):
-            vectorizer =  TfidfVectorizer(ngram_range=n_gram, max_df=0.9, min_df=words_to_remove, stop_words='english')
-            X_train = vectorizer.fit_transform(train)
-            X_test = vectorizer.transform(test)
-            feature_names = vectorizer.get_feature_names_out()
-            return X_train, X_test, feature_names
+list_models = []
+for n_gram in [(1,1), (1,2)]:
+    for words_to_remove in [5, 6, 7, 8, 9, 10, 15, 18, 19, 20, 21, 22, 23, 24]:
+        for iter in (100, 250, 500, 750, 1000, 5000):
+            def transform(train, test):
+                vectorizer =  TfidfVectorizer(ngram_range=n_gram, max_df=0.9, min_df=words_to_remove, stop_words='english')
+                X_train = vectorizer.fit_transform(train)
+                X_test = vectorizer.transform(test)
+                feature_names = vectorizer.get_feature_names_out()
+                return X_train, X_test, feature_names
 
 
-        X_train, X_test, feature_names = transform(combined_reviews_training['review'], combined_reviews_testing['review'])
-        y_train = combined_reviews_training['label']
-        y_test = combined_reviews_testing['label']
+            X_train, X_test, feature_names = transform(combined_reviews_training['review'], combined_reviews_testing['review'])
+            y_train = combined_reviews_training['label']
+            y_test = combined_reviews_testing['label']
 
-        # use CV for built-in cross validation
-        regress = LogisticRegressionCV(penalty='l1', solver='liblinear')
-        regress.fit(X_train, y_train)
-        y_pred = regress.predict(X_test)
+            # use CV for built-in cross validation
+            # Lasso: l1 penalty with liblinear solver
+            regress = LogisticRegressionCV(penalty='l1', solver='liblinear', max_iter=iter)
+            regress.fit(X_train, y_train)
+            y_pred = regress.predict(X_test)
 
-        coefficients = regress.coef_[0]
-        feature_importance = pd.DataFrame({'Feature': feature_names, 'Importance': np.abs(coefficients)})
-        feature_importance = feature_importance.sort_values('Importance', ascending=False)
-        scores = classification_report(y_test, y_pred)
-        print(n_gram, words_to_remove, scores, feature_importance[:5])
+            coefficients = regress.coef_[0]
+            feature_importance = pd.DataFrame({'Feature': feature_names, 'Importance': coefficients})
+            feature_importance_pos = feature_importance.sort_values('Importance', ascending=False)[:5]
+            feature_importance_neg = feature_importance.sort_values('Importance', ascending=True)[:5]
+            scores = classification_report(y_test, y_pred)
+            acc = accuracy_score(y_test, y_pred)
+            pre = round(precision_score(y_test, y_pred), 3)
+            rec = round(recall_score(y_test, y_pred), 3)
+            f1 = round(f1_score(y_test, y_pred), 3)
+            c_value = regress.C_
+            list_models.append([n_gram, words_to_remove, iter, c_value, acc, pre, rec, f1])
+        if n_gram == (1,2) and words_to_remove in (22,23):
+            print('real', feature_importance_pos['Feature'].values,'fake', feature_importance_neg['Feature'].values)
+print(*list_models, sep='\n')
