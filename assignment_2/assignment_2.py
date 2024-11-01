1874a44881779c2f3ef319150412830ba8441725
Annebelle Olminkhof <20630578+annebelleo@users.noreply.github.com>
2024-11-01 15:41:35 +0100

    add code plots

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -1,14 +1,15 @@
 import os
 import pandas as pd
 import numpy as np
-import nltk
+import matplotlib as plt
+import seaborn as sns
 from nltk.tokenize import word_tokenize
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 data_folder = os.path.join(os.getcwd(), 'assignment_2/data/')
@@ -48,38 +49,55 @@ combined_reviews_testing = pd.concat([deceptive_reviews_testing, truthful_review
 
 # pre-processing
 list_models = []
-for n_gram in [(1,1), (1,2)]:
-    for words_to_remove in [5, 6, 7, 8, 9, 10, 15, 18, 19, 20, 21, 22, 23, 24]:
-        for iter in (100, 250, 500, 750, 1000, 5000):
-            def transform(train, test):
-                vectorizer =  TfidfVectorizer(ngram_range=n_gram, max_df=0.9, min_df=words_to_remove, stop_words='english')
-                X_train = vectorizer.fit_transform(train)
-                X_test = vectorizer.transform(test)
-                feature_names = vectorizer.get_feature_names_out()
-                return X_train, X_test, feature_names
+for n_gram in [(1,2)]:
+    for words_to_remove in (range(2, 30)):
+        def transform(train, test):
+            # strip punctuation
+            train = train.str.replace('[^\w\s]','')
+            test = test.str.replace('[^\w\s]','')
+            vectorizer =  TfidfVectorizer(ngram_range=n_gram, max_df=0.9, min_df=words_to_remove, stop_words='english')
+            X_train = vectorizer.fit_transform(train)
+            X_test = vectorizer.transform(test)
+            feature_names = vectorizer.get_feature_names_out()
+            return X_train, X_test, feature_names
 
+        X_train, X_test, feature_names = transform(combined_reviews_training['review'], combined_reviews_testing['review'])
+        y_train = combined_reviews_training['label']
+        y_test = combined_reviews_testing['label']
 
-            X_train, X_test, feature_names = transform(combined_reviews_training['review'], combined_reviews_testing['review'])
-            y_train = combined_reviews_training['label']
-            y_test = combined_reviews_testing['label']
+        # use CV for built-in cross validation
+        # Lasso: l1 penalty with liblinear solver
+        regress = LogisticRegressionCV(penalty='l1', solver='liblinear')
+        regress.fit(X_train, y_train)
+        y_pred = regress.predict(X_test)
 
-            # use CV for built-in cross validation
-            # Lasso: l1 penalty with liblinear solver
-            regress = LogisticRegressionCV(penalty='l1', solver='liblinear', max_iter=iter)
-            regress.fit(X_train, y_train)
-            y_pred = regress.predict(X_test)
+        coefficients = regress.coef_[0]
+        feature_importance = pd.DataFrame({'Feature': feature_names, 'Importance': coefficients})
+        feature_importance_pos = feature_importance.sort_values('Importance', ascending=False)[:5]
+        feature_importance_neg = feature_importance.sort_values('Importance', ascending=True)[:5]
 
-            coefficients = regress.coef_[0]
-            feature_importance = pd.DataFrame({'Feature': feature_names, 'Importance': coefficients})
-            feature_importance_pos = feature_importance.sort_values('Importance', ascending=False)[:5]
-            feature_importance_neg = feature_importance.sort_values('Importance', ascending=True)[:5]
-            scores = classification_report(y_test, y_pred)
-            acc = accuracy_score(y_test, y_pred)
-            pre = round(precision_score(y_test, y_pred), 3)
-            rec = round(recall_score(y_test, y_pred), 3)
-            f1 = round(f1_score(y_test, y_pred), 3)
-            c_value = regress.C_
-            list_models.append([n_gram, words_to_remove, iter, c_value, acc, pre, rec, f1])
-        if n_gram == (1,2) and words_to_remove in (22,23):
-            print('real', feature_importance_pos['Feature'].values,'fake', feature_importance_neg['Feature'].values)
-print(*list_models, sep='\n')
+        scores = classification_report(y_test, y_pred)
+        acc = accuracy_score(y_test, y_pred)
+        pre = round(precision_score(y_test, y_pred), 4)
+        rec = round(recall_score(y_test, y_pred), 4)
+        f1 = round(f1_score(y_test, y_pred), 4)
+        c_value = regress.C_
+        list_models.append([n_gram, words_to_remove, c_value, acc, pre, rec, f1])
+        if n_gram == (1,2) and words_to_remove == 22:
+            cm = confusion_matrix(y_test, y_pred)
+            plot = sns.heatmap(cm, annot=True)
+            fig = plot.get_figure()
+            fig.savefig('confusion')
+            fig.clf()
+
+            feature_plot = sns.barplot(feature_importance_pos, x='Feature', y='Importance')
+            feature_plot.set_title('Top 5 features for class 1')
+            fig = feature_plot.get_figure()
+            fig.savefig('feature_logistic_pos')
+            fig.clf()
+
+            feature_plot = sns.barplot(feature_importance_neg, x='Feature', y='Importance')
+            feature_plot.set_title('Top 5 features for class 0')
+            fig = feature_plot.get_figure()
+            fig.savefig('feature_logistic_neg')
+# print(*list_models, sep='\n')
